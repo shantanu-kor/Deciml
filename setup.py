c5917cccb0fccef4d8845d515842ac289370683e
shantanu-kor <kor.shantanu1@gmail.com>
2024-12-27 00:32:34 +0530

    made changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Deciml",
-    version="0.1.1",
+    version="0.1.2",
     description="Extension of Decimal standard package",
     author="Shank",
     author_email="kor.shantanu1@gmail.com",
